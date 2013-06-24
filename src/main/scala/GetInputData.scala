e732a337a6deb3ba0c69fbf569ae8a6e603233bd
Ronald Crooy <r.a.crooy@gmail.com>
2013-06-24 21:21:51 +0200

    finally fixed scaling

--- src/main/scala/GetInputData.scala
+++ src/main/scala/GetInputData.scala
@@ -1,21 +1,30 @@
 package com.crooy
 
+import java.awt.Image
+import java.io._
+import java.net.URL
+import javax.imageio.ImageIO
+import java.awt.image._
+
 object GetInputData {
-  import java.io._
-  import java.net.URL
-  import javax.imageio.ImageIO
-  import java.awt.image._
  
   def ReadFile(file:URL)={
 	ImageIO.read(file)
   } 
   
+  def Scale(image:BufferedImage, size:Int)={
+    val scaled = image.getScaledInstance(size, -1, Image.SCALE_SMOOTH)
+	val buffered = new BufferedImage(scaled.getWidth(null), scaled.getHeight(null), BufferedImage.TYPE_INT_RGB);
+	buffered.getGraphics().drawImage(image, 0, 0 , null);
+	buffered
+  }
 
   def ConvertToBlackAndWhite(bufImg:Raster)={
     val width = bufImg.getWidth();val height = bufImg.getHeight();
     for(x <- 0 until width; y <- 0 until height) yield{
-    	val pixel = bufImg.getPixel(x, y, new Array[Int](1))
-        if (pixel(0) < 150) 1  //the magic number that seperates black from white
+    	val pixel = bufImg.getPixel(x, y, new Array[Int](3))
+    	val sumPixel = pixel(0) + pixel(1) + pixel(2)
+        if (sumPixel < 200) 1  //the magic number that seperates black from white
         else 0 	
     }
   }
--- src/test/scala/GetInputDataTest.scala
+++ src/test/scala/GetInputDataTest.scala
@@ -23,10 +23,17 @@ class GetInputDataTest extends FunSuite {
   }
 
   test("Can read files into less pixels than it's original size"){
-    val data = GetInputData.ReadFile(getClass.getResource("/sun.png")).getData()
-    val pixels = GetInputData.ConvertToBlackAndWhite(data)
-    assert(pixels.forall(b => b == 0 || b == 1))
-    assert(pixels.count(_ == 1) > 10) // assume that there are more than 10 black pixels
+    val data = GetInputData.ReadFile(getClass.getResource("/sun.png"))
+    val scaledData = GetInputData.Scale(data, 100)
+    assert(scaledData.getHeight() == 100)
+    assert(scaledData.getWidth() == 100)
+  }
+  test("Can read files into less pixels than it's original size in 1s and 0s"){
+    val data = GetInputData.ReadFile(getClass.getResource("/sun.png"))
+    val scaledData = GetInputData.Scale(data, 100)
+    val pixels = GetInputData.ConvertToBlackAndWhite(scaledData.getData())
+    //expecting 1111 x 1111 image
+    assert(100 * 100 == pixels.length)
   }
 
 }
